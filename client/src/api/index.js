8256c84922274b22aa6ecffb615b097ae2c1da68
Vlad <vladsa000@gmail.com>
2023-02-01 22:24:14 +0200

    make it better

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,42 +1,45 @@
 import axios from "axios";
-import LocalStorageSerive from 'Services/localStorage';
-import authAPI from 'api/auth';
+import LocalStorageSerive from "Services/localStorage";
+import authAPI from "api/auth";
 import { store } from "redux/store";
 import { setUser, removeUser } from "redux/user/slice";
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = "https://discord-clone-express.herokuapp.com";
 
 const apiClient = axios.create({
-    baseURL: BASE_URL,
-    timeout: 1000,
-    withCredentials: true
+  baseURL: BASE_URL,
+  timeout: 1000,
+  withCredentials: true,
 });
 
-apiClient.interceptors.request.use(config => {
-    const token = LocalStorageSerive.getToken();
-    
-    if(token) {
-        config.headers.authorization = `Bearer ${token}`;
-    }
+apiClient.interceptors.request.use((config) => {
+  const token = LocalStorageSerive.getToken();
 
-    return config
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
-apiClient.interceptors.response.use((config) => {
+apiClient.interceptors.response.use(
+  (config) => {
     return config;
-},async (error) => {
+  },
+  async (error) => {
     const originalRequest = error.config;
     if (error.response.status === 401 && !error.config._isRetry) {
-        originalRequest._isRetry = true;
-        try {
-            const response =  await authAPI.refresh();
-            store.dispatch(setUser({...response.data}));
-            return apiClient.request(originalRequest);
-        } catch (e) {
-            store.dispatch(removeUser());
-        }
+      originalRequest._isRetry = true;
+      try {
+        const response = await authAPI.refresh();
+        store.dispatch(setUser({ ...response.data }));
+        return apiClient.request(originalRequest);
+      } catch (e) {
+        store.dispatch(removeUser());
+      }
     }
     throw error;
-})
+  }
+);
 
-export default apiClient
+export default apiClient;
